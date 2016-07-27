1d32ced5fb85f3a33d9a41be8e1747f02213c321
daiiz <da.iz.mg@gmail.com>
2016-07-27 12:08:02 +0900

    全画面になった

--- app/src/main/java/app/me/daiz/shokujincamera/MainActivity.java
+++ app/src/main/java/app/me/daiz/shokujincamera/MainActivity.java
@@ -2,12 +2,19 @@ package app.me.daiz.shokujincamera;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+
+
+
         setContentView(R.layout.activity_main);
     }
 }
