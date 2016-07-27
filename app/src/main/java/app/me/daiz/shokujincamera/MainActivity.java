f3434339f30d34d28b7536eb2b50a3e3db5f87a6
daiiz <da.iz.mg@gmail.com>
2016-07-27 12:30:00 +0900

    カメラ映像拾える（向きが変だけど） 実機設定の権限与えないと起動しないの注意 java.lang.RuntimeException: Fail to connect to camera service

--- app/src/main/java/app/me/daiz/shokujincamera/MainActivity.java
+++ app/src/main/java/app/me/daiz/shokujincamera/MainActivity.java
@@ -1,11 +1,23 @@
 package app.me.daiz.shokujincamera;
 
+import android.hardware.Camera;
+import android.hardware.Camera.Parameters;
+import android.hardware.Camera.Size;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.FrameLayout;
+import android.widget.FrameLayout.LayoutParams;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+    SurfaceView   sview;
+    SurfaceHolder sholder;
+    Camera camera;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,8 +25,55 @@ public class MainActivity extends AppCompatActivity {
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
 
+        FrameLayout frame = new FrameLayout(this);
+        setContentView(frame);
 
+        sview = new SurfaceView(this);
+        sholder = sview.getHolder();
+        sholder.addCallback(new SurfaceHolderCallback());
 
-        setContentView(R.layout.activity_main);
+        frame.addView(sview);
+        //setContentView(R.layout.activity_main);
+    }
+
+    class SurfaceHolderCallback implements SurfaceHolder.Callback {
+        @Override
+        public void surfaceCreated(SurfaceHolder holder) {
+            camera = Camera.open();
+            Parameters param = camera.getParameters();
+            List<Size> ss = param.getSupportedPictureSizes();
+            Size pictureSize = ss.get(0);
+            param.setPictureSize(pictureSize.width, pictureSize.height);
+            camera.setParameters(param);
+        }
+
+        @Override
+        public void surfaceChanged(SurfaceHolder holder, int f, int w, int h) {
+            try {
+                camera.setDisplayOrientation(0);
+                camera.setPreviewDisplay(sview.getHolder());
+
+                Parameters param = camera.getParameters();
+                List<Size> previewSizes = camera.getParameters().getSupportedPreviewSizes();
+                Size preview = previewSizes.get(0);
+                param.setPreviewSize(preview.width, preview.height);
+
+                LayoutParams lp = new LayoutParams(preview.width, preview.height);
+                sview.setLayoutParams(lp);
+
+                camera.setParameters(param);
+                camera.startPreview();
+
+            } catch (Exception e) {
+                System.out.print(e);
+            }
+        }
+
+        @Override
+        public void surfaceDestroyed(SurfaceHolder holder) {
+            camera.stopPreview();
+            camera.release();
+        }
     }
 }
+
